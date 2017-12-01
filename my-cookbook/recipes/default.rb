fec1b7a7d5daf71933e77922173a7e422f334e24
Đăng LV <danglv10688@gmail.com>
2017-12-01 10:24:31 +0700

    cookbook

---
+++ my-cookbook/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook:: my-cookbook
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+node.default['packages-cookbook'] = [
+ 'nodejs',
+ 'imagemagick',
+ 'postgresql'
+]
